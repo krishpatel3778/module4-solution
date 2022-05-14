da10afe6e1bddfadf0094c8bd47a8fcfbec795dc
krishpatel3778 <patelrkrish@icloud.com>
2022-05-14 18:38:04 -0400

    done

--- script.js
+++ script.js
@@ -33,8 +33,18 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
+(function(window){
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  for(var i=0;i<names.length;i++){
+  if(var[i].substring(0,1).toLowerCase()=="j"){
+    helloSpeaker.speak(var[i]);
+  }else{
+    byeSpeaker.speak(var[i]);
+  }
+  window.names=names;
+}
+})(window);
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
